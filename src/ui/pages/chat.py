b48539c109674e9e6dd09d897065d16c73894d76
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2023-12-18 00:08:02 -0300

    main chat page

---
+++ src/ui/pages/chat.py
@@ -0,0 +1,56 @@
+import os, sys
+import streamlit as st
+import uuid
+
+sys.path.append(os.getcwd())
+from ui.src.components import (
+    check_user_login,
+    display_session_history,
+    initiate_session_state,
+    page_config,
+    sidebar,
+)
+
+page_config()
+check_user_login()
+
+
+def chat():
+    
+    filters = sidebar()
+    initiate_session_state(filters)
+
+    st.title("Chatbot de Notícias - Poços de Caldas")
+
+    display_session_history()
+
+    if prompt := st.chat_input("Digite aqui..."):
+        st.session_state["messages"].append({"role": "user", "response": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        response = {"response": f"Echo: {prompt}", "execution_id": uuid.uuid4().hex}
+        with st.chat_message("assistant"):
+            st.markdown(response["response"])
+        st.session_state["messages"].append(
+            {
+                "role": "assistant",
+                "response": response["response"],
+                "execution_id": response["execution_id"],
+            }
+        )
+        st.session_state["execution_ids"].update(
+            {
+                response["execution_id"]: {
+                    "message": prompt,
+                    "response": response["response"],
+                    "filters": filters,
+                }
+            }
+        )
+
+        st.rerun()
+
+
+if __name__ == "__main__":
+    chat()
