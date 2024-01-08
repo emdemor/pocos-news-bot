02780ed48fcea605bcf6d0a04b30c4846e5e3e23
Eduardo Morais <emdemor415@gmail.com>
2024-01-08 00:00:39 -0300

    add context search

--- src/bot/handlers/_base.py
+++ src/bot/handlers/_base.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from importlib import resources
 from typing import List
 
+import tiktoken
 from langchain.prompts import PromptTemplate
 from langchain.chains.base import Chain
 from langchain_core.language_models.chat_models import BaseChatModel
@@ -11,6 +12,7 @@ from langchain_core.messages.ai import AIMessage
 from langchain.chains import LLMChain
 
 from bot import BotConfig
+from bot.data_models import News, VectorDatabaseNewsResult
 
 config = BotConfig()
 
@@ -31,6 +33,10 @@ class Handler(ABC):
         params = {}
         if self.use_chat_history:
             params.update(dict(history=self.get_chat_history()))
+
+        if self.use_context:
+            params.update(dict(context=self.get_context(message)))
+
         return self.chain.predict(human_input=message, **params)
 
     @property
@@ -64,6 +70,11 @@ class Handler(ABC):
         pass
 
     @property
+    @abstractmethod
+    def vector_database(self):
+        pass
+
+    @property
     def prompt(self) -> PromptTemplate:
         return self.get_prompt(self.prompt_key)
 
@@ -74,25 +85,90 @@ class Handler(ABC):
 
     @property
     @abstractmethod
-    def use_chat_history(self) -> Chain:
+    def use_chat_history(self) -> bool:
         pass
-    
+
+    @property
+    @abstractmethod
+    def use_context(self) -> bool:
+        pass
+
+    @property
+    @abstractmethod
+    def llm_context_window_size(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def prompt_max_tokens(self) -> int:
+        pass
 
     def get_chat_history(self) -> str:
         _class = self.__class__
-        if hasattr(_class, "memory"):
-            return "".join(
-                self.format_history_message(self.memory.chat_memory.messages)
-            )
-        raise AttributeError(f"The class {_class.__name__} does not have the 'memory' attribute")
 
-    def format_history_message(self, messages: List[HumanMessage | AIMessage]):
+        if not hasattr(_class, "memory"):
+            raise AttributeError(
+                f"The class {_class.__name__} does not have the 'memory' attribute"
+            )
+
+        if not self.use_chat_history:
+            raise ValueError(
+                f"You cannot get the chat history if 'use_chat_history=False'"
+            )
+
+        return "".join(self._format_history_message(self.memory.chat_memory.messages))
+
+    def get_context(self, query, n_results=10, n_neighbors=1000):
+        _class = self.__class__
+
+        if not hasattr(_class, "vector_database"):
+            raise AttributeError(
+                f"The class {_class.__name__} does not have the 'vector_database' attribute"
+            )
+
+        if not self.use_context:
+            raise ValueError(f"You cannot get context if 'use_context=False'")
+
+        results = self.vector_database.get_most_similar(
+            query, n_neighbors=n_neighbors, n_results=n_results
+        )
+        return self._set_query_content(results)
+
+    def _format_history_message(self, messages: List[HumanMessage | AIMessage]):
         for message in messages:
             if isinstance(message, HumanMessage):
                 yield f"{config.HUMAN_PREFIX}: {message.content}\n"
-    
+
             elif isinstance(message, AIMessage):
                 yield f"{config.AI_PREFIX}: {message.content}\n"
+
+    def _count_tokens(self, context: str) -> int:
+        encoding = tiktoken.encoding_for_model(self.llm_model)
+        num_tokens = len(encoding.encode(context))
+        return num_tokens
+
+    @staticmethod
+    def _format_news(news: News, order: int) -> str:
+        return (
+            f"\n<noticia_{order}>\n"
+            f"\t<data>{news.date}</data>\n"
+            f"\t<titulo>{news.title}</titulo>\n"
+            f"\t<autor>{news.author}</autor>\n"
+            f"\t<link>{news.link}</link>\n"
+            f"\t<conteudo>{news.document}</conteudo>\n"
+            f"</noticia_{order}>"
+        )
+
+    def _set_query_content(self, results: List[VectorDatabaseNewsResult]) -> str:
+        context_list = [self._format_news(res.news, i+1) for i, res in enumerate(results)]
+        tokens = self.llm_context_window_size
+        max_tokens = self.prompt_max_tokens
+        while tokens > max_tokens:
+            tokens = self._count_tokens("\n".join(context_list))
+            if tokens >= max_tokens:
+                context_list.pop()
+        return "\n".join(sorted(context_list))
+
 
 class PrivateHandler(Handler, ABC):
     @property
@@ -102,6 +178,7 @@ class PrivateHandler(Handler, ABC):
             prompt=self.prompt,
             verbose=self.verbose,
         )
+
 
 class PublicHandler(Handler, ABC):
     @property
