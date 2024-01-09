06731789083fe5fd75d7e0a65cf653e905f3d429
Eduardo Morais <emdemor415@gmail.com>
2024-01-08 23:11:27 -0300

    add new handlers

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -2,6 +2,8 @@ from bot.handlers._base import Handler, PrivateHandler, PublicHandler
 from bot.handlers._standalone_handler import StandaloneHandler
 from bot.handlers._intention_handler import IntentionHandler
 from bot.handlers._query_handler import QueryHandler
+from bot.handlers._fallback_handler import FallbackHandler
+from bot.handlers._greeting_handler import GreetingHandler
 
 __all__ = [
     "Handler",
@@ -10,4 +12,6 @@ __all__ = [
     "StandaloneHandler",
     "IntentionHandler",
     "QueryHandler",
+    "FallbackHandler",
+    "GreetingHandler",
 ]
